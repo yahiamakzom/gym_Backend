548179b24a2821b48c2cb98a3f8775d3960c61e9
yahiamakzom <138879397+yahiamakzom@users.noreply.github.com>
2023-08-25 14:38:22 +0300

    Update Map.js add new distance method

--- utils/Map.js
+++ utils/Map.js
@@ -37,6 +37,28 @@ exports.calcDistance = async (origin, destination) => {
     }
 };
 
+exports.calculateDistance = (lat1, lon1, lat2, lon2) => {
+  const earthRadius = 6371; // Earth's radius in kilometers
+
+  const dLat = degreesToRadians(lat2 - lat1);
+  const dLon = degreesToRadians(lon2 - lon1);
+
+  const a = Math.pow(Math.sin(dLat / 2), 2) +
+    Math.cos(degreesToRadians(lat1)) *
+    Math.cos(degreesToRadians(lat2)) *
+    Math.pow(Math.sin(dLon / 2), 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  let distance = earthRadius * c;
+  distance = parseFloat(distance.toFixed(2));
+  return distance;
+}
+
+function degreesToRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
+
 
 exports.getLocationName = async (latitude, longitude) => {
     let location;
