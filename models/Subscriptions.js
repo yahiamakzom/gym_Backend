1c2aa88a3579d79f1dab0139b358505abd85cee0
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-02-19 09:37:31 +0200

    ...

--- models/Subscriptions.js
+++ models/Subscriptions.js
@@ -1,26 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports = mongoose.model("Subscriptions", new mongoose.Schema({
-    club: {
+module.exports = mongoose.model(
+  "Subscriptions",
+  new mongoose.Schema(
+    {
+      club: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Club"
-    },
-    name: {
+        ref: "Club",
+      },
+      name: {
         type: String,
-        required: [true, "Enter Subscription Name"]
-    },
-    price: {
+        required: [true, "Enter Subscription Name"],
+      },
+      freezeTime: {
         type: Number,
-        required: [true, "Enter Subscription Name"]
-    },
-    type: {
+      },
+      freezeCountTime: {
+        type: Number,
+      },
+      price: {
+        type: Number,
+        required: [true, "Enter Subscription Name"],
+      },
+      type: {
         type: String,
         enum: ["سنوي", "شهري", "اسبوعي", "يومي"],
-        required: [true,"Please Add a Subscription Date"]
-    },
-    numberType:{
+        required: [true, "Please Add a Subscription Date"],
+      },
+      numberType: {
         type: Number,
-        default:0,
-        required: [true, "Enter Subscription number for sepcicfic volumn type"]
-    }
-}, { timestamps: true }))
+        default: 0,
+        required: [true, "Enter Subscription number for sepcicfic volumn type"],
+      },
+    },
+    { timestamps: true }
+  )
+);
