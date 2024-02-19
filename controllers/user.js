8baf163d75495b5089ea21f0e993b51735fc8c29
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-02-19 20:57:02 +0200

    ..

--- controllers/user.js
+++ controllers/user.js
@@ -922,7 +922,7 @@ exports.userFreezing = asyncHandler(async (req, res, next) => {
 
     res
       .status(200)
-      .json({ message: "User subscription frozen successfully", newEndDate });
+      .json({ message: "User subscription frozen successfully", newEndDate ,freezenData });
   } catch (error) {
     next(error);
   }
