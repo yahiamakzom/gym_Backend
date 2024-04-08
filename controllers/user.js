620a380123e8fcc164271a84cfd9081e0a53bb55
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-04-09 01:48:45 +0200

    ...

--- controllers/user.js
+++ controllers/user.js
@@ -852,18 +852,17 @@ exports.checkPayment = asyncHandler(async (req, res, next) => {
                 for (const sub of allClubSubscriptions) {
                   // Check if sub.endData is before subscription.endData OR sub.StartData is the same as subscription.startData
                   if (
-                    (moment(sub.endData)
+                    moment(sub.endData)
                       .startOf("day")
-                      .isBefore(moment(subscription.endData).startOf("day")) ||
-                      moment(sub.endData)
-                        .startOf("day")
-                        .isSame(moment(subscription.endData).startOf("day"))) &&
-                    (moment(sub.startData)
+                      .isSame(moment(subscription.endData).startOf("day"))
+                  ) {
+                    sub.gymsCount = 0;
+                    await sub.save();
+                  }
+                  if (
+                    moment(sub.startData)
                       .startOf("day")
-                      .isSame(moment(subscription.startData).startOf("day")) ||
-                      moment(sub.startData)
-                        .startOf("day")
-                        .isAfter(moment(subscription.startData).startOf("day")))
+                      .isSame(moment(subscription.startData).startOf("day"))
                   ) {
                     sub.gymsCount = 0;
                     await sub.save();
@@ -1802,22 +1801,24 @@ exports.subscriptionConfirmation = asyncHandler(async (req, res, next) => {
       });
 
       for (const sub of allClubSubscriptions) {
-        if (
-          (moment(sub.endData)
-            .startOf("day")
-            .isBefore(moment(subscription.endData).startOf("day")) ||
+        for (const sub of allClubSubscriptions) {
+          // Check if sub.endData is before subscription.endData OR sub.StartData is the same as subscription.startData
+          if (
             moment(sub.endData)
               .startOf("day")
-              .isSame(moment(subscription.endData).startOf("day"))) &&
-          (moment(sub.startData)
-            .startOf("day")
-            .isSame(moment(subscription.startData).startOf("day")) ||
+              .isSame(moment(subscription.endData).startOf("day"))
+          ) {
+            sub.gymsCount = 0;
+            await sub.save();
+          }
+          if (
             moment(sub.startData)
               .startOf("day")
-              .isAfter(moment(subscription.startData).startOf("day")))
-        ) {
-          sub.gymsCount = 0;
-          await sub.save();
+              .isSame(moment(subscription.startData).startOf("day"))
+          ) {
+            sub.gymsCount = 0;
+            await sub.save();
+          }
         }
       }
     }
