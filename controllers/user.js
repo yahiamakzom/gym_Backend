25a6edbd72150125cdd6175160a18faa44708b8b
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-04-09 02:18:17 +0200

    ...

--- controllers/user.js
+++ controllers/user.js
@@ -852,17 +852,10 @@ exports.checkPayment = asyncHandler(async (req, res, next) => {
                 for (const sub of allClubSubscriptions) {
                   // Check if sub.endData is before subscription.endData OR sub.StartData is the same as subscription.startData
                   if (
-                    moment(sub.endData)
-                      .startOf("day")
-                      .isSame(moment(subscription.endData).startOf("day"))
-                  ) {
-                    sub.gymsCount = 0;
-                    await sub.save();
-                  }
-                  if (
-                    moment(sub.startData)
-                      .startOf("day")
-                      .isSame(moment(subscription.startData).startOf("day"))
+                    (moment(sub.endData).isBefore(subscription.endData) ||
+                      moment(sub.endData).isSame(subscription.endData)) &&
+                    (moment(sub.startData).isSame(subscription.startData) ||
+                      moment(sub.startData).isAfter(subscription.startData))
                   ) {
                     sub.gymsCount = 0;
                     await sub.save();
@@ -1801,24 +1794,14 @@ exports.subscriptionConfirmation = asyncHandler(async (req, res, next) => {
       });
 
       for (const sub of allClubSubscriptions) {
-        for (const sub of allClubSubscriptions) {
-          // Check if sub.endData is before subscription.endData OR sub.StartData is the same as subscription.startData
-          if (
-            moment(sub.endData)
-              .startOf("day")
-              .isSame(moment(subscription.endData).startOf("day"))
-          ) {
-            sub.gymsCount = 0;
-            await sub.save();
-          }
-          if (
-            moment(sub.startData)
-              .startOf("day")
-              .isSame(moment(subscription.startData).startOf("day"))
-          ) {
-            sub.gymsCount = 0;
-            await sub.save();
-          }
+        if (
+          (moment(sub.endData).isBefore(subscription.endData) ||
+            moment(sub.endData).isSame(subscription.endData)) &&
+          (moment(sub.startData).isSame(subscription.startData) ||
+            moment(sub.startData).isAfter(subscription.startData))
+        ) {
+          sub.gymsCount = 0;
+          await sub.save();
         }
       }
     }
