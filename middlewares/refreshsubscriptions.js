47f6ddf476776c42edc29faf33e12ad1b7216397
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-04-12 09:26:25 +0200

    ..addd now

--- middlewares/refreshsubscriptions.js
+++ middlewares/refreshsubscriptions.js
@@ -4,90 +4,85 @@ const Club = require("../models/Club");
 const moment = require("moment");
 
 module.exports = expressAsyncHandler(async (req, res, next) => {
-  try {
-    const subscriptionTypes = ["90Minutes", "30Minutes", "60Minutes"];
-    const subscriptions = await Subscription.find({
-      type: { $in: subscriptionTypes },
-    });
-    const clubIds = [
-      ...new Set(subscriptions.map((subscription) => subscription.club)),
-    ];
-    const clubs = await Club.find({
-      _id: { $in: clubIds },
-    });
+  const subscriptionTypes = ["90Minutes", "30Minutes", "60Minutes"];
+  const subscriptions = await Subscription.find({
+    type: { $in: subscriptionTypes },
+  });
+  const clubIds = [
+    ...new Set(subscriptions.map((subscription) => subscription.club)),
+  ];
+  const clubs = await Club.find({
+    _id: { $in: clubIds },
+  });
 
-    for (const club of clubs) {
-      const clubSubscriptions = subscriptions.filter(
-        (subscription) => subscription.club.toString() === club._id.toString()
-      );
-      if (club.to && club.from && moment(club.to, "HH:mm", true).isValid()) {
-        if (moment().isAfter(moment(club.to, "HH:mm"))) {
-          for (const subscription of clubSubscriptions) {
-            const formattedEndDate = moment
-              .utc(subscription.endData)
-              .format("YYYY-MM-DD HH:mm:ss");
-            const formattedCurrentDate = moment().format("YYYY-MM-DD HH:mm:ss");
-            const isAfterEndDate = moment(formattedCurrentDate).isAfter(
-              moment(formattedEndDate)
+  for (const club of clubs) {
+    const clubSubscriptions = subscriptions.filter(
+      (subscription) => subscription.club.toString() === club._id.toString()
+    );
+    if (club.to && club.from && moment(club.to, "HH:mm", true).isValid()) {
+      if (moment().isAfter(moment(club.to, "HH:mm"))) {
+        for (const subscription of clubSubscriptions) {
+          const formattedEndDate = moment
+            .utc(subscription.endData)
+            .format("YYYY-MM-DD HH:mm:ss");
+          const formattedCurrentDate = moment().format("YYYY-MM-DD HH:mm:ss");
+          const isAfterEndDate = moment(formattedCurrentDate).isAfter(
+            moment(formattedEndDate)
+          );
+          if (isAfterEndDate) {
+            // Perform desired operations
+            subscription.endData = moment(subscription.endData).add(1, "day");
+            subscription.startData = moment(subscription.startData).add(
+              1,
+              "day"
             );
-            if (isAfterEndDate) {
-              // Perform desired operations
-              subscription.endData = moment(subscription.endData).add(1, "day");
-              subscription.startData = moment(subscription.startData).add(
-                1,
-                "day"
-              );
-              subscription.gymsCount = subscription.gymsCountFixed;
-              await subscription.save();
-              console.log(subscription);
-            }
-
-            console.log("sdcsdvs");
-            console.log(formattedCurrentDate);
-            console.log(formattedEndDate);
+            subscription.gymsCount = subscription.gymsCountFixed;
+            await subscription.save();
             console.log(subscription);
           }
-        } else {
-          console.log(moment().isAfter(moment(club.to, "HH:mm")));
-          console.log("not end");
+
+          console.log("sdcsdvs");
+          console.log(formattedCurrentDate);
+          console.log(formattedEndDate);
+          console.log(subscription);
         }
       } else {
-        const now = moment();
-        const startOfDay = now.startOf("day");
-      
+        console.log(moment().isAfter(moment(club.to, "HH:mm")));
+        console.log("not end");
+      }
+    } else {
+      const now = moment();
+      const startOfDay = now.startOf("day");
 
-        if (now.isSame(startOfDay)) {
-          for (const subscription of clubSubscriptions) {
-            const formattedEndDate = moment
-              .utc(subscription.endData)
-              .format("YYYY-MM-DD HH:mm:ss");
-            const formattedCurrentDate = moment().format("YYYY-MM-DD HH:mm:ss");
-            const isAfterEndDate = moment(formattedCurrentDate).isAfter(
-              moment(formattedEndDate)
+      if (now.isSame(startOfDay)) {
+        for (const subscription of clubSubscriptions) {
+          const formattedEndDate = moment
+            .utc(subscription.endData)
+            .format("YYYY-MM-DD HH:mm:ss");
+          const formattedCurrentDate = moment().format("YYYY-MM-DD HH:mm:ss");
+          const isAfterEndDate = moment(formattedCurrentDate).isAfter(
+            moment(formattedEndDate)
+          );
+          if (isAfterEndDate) {
+            // Perform desired operations
+            subscription.endData = moment(subscription.endData).add(1, "day");
+            subscription.startData = moment(subscription.startData).add(
+              1,
+              "day"
             );
-            if (isAfterEndDate) {
-              // Perform desired operations
-              subscription.endData = moment(subscription.endData).add(1, "day");
-              subscription.startData = moment(subscription.startData).add(
-                1,
-                "day"
-              );
-              subscription.gymsCount = subscription.gymsCountFixed;
-              await subscription.save();
-              console.log(subscription);
-            }
+            subscription.gymsCount = subscription.gymsCountFixed;
+            await subscription.save();
+            console.log(subscription);
           }
         }
-        console.log("asdcasd");
-    
-        console.log("sdsd");
-  
-        console.log(moment());
       }
-    }
+      console.log("asdcasd");
 
-    next();
-  } catch (error) {
-    next(error);
+      console.log("sdsd");
+
+      console.log(moment());
+    }
   }
+
+  next();
 });
