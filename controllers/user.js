fd1452ee6454ae00db6fd93d05ca365652978196
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2024-05-02 14:03:28 +0300

    reset password

--- controllers/user.js
+++ controllers/user.js
@@ -1998,7 +1998,9 @@ exports.resetPassowrd = asyncHandler(async (req, res) => {
   if (!user) {
     return res.status(404).json({ error: "User not found" ,success:false});
   }
-
+ if(password.length>6){ 
+  return res.status(404).json({ error: "Password should be at least 6 characters" ,success:false});
+ }
   const hashedPassword = await bcrypt.hash(password, 10);
   user.password = hashedPassword;
   await user.save();
